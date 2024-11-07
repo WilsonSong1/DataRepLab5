c430b7acecc72ff8271ec97e68621d68c34521eb
WilsonSong1 <G00418330@atu.ie>
2024-11-07 19:04:21 +0000

    Exercise 4 made a route for /hello/John and /hello/John/Doe as a UML paramater

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`Hello ${name} ${surname}`);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
